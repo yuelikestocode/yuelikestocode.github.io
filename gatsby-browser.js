c282dc85c57e75f00d11ccd9f4981dec8f5898e5
Yue Yu <yue.yu.career@gmail.com>
2022-08-31 17:42:51 -0700

    article, code styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,11 +2,13 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import './src/styles/global.css'
+import "./src/styles/global.css"
 // // normalize CSS across browsers
 // import "./src/normalize.css"
 // // custom CSS styles
 // import "./src/style.css"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+// import "prismjs/themes/prism.css"
+// require('./src/styles/prism.css')
+import "./src/styles/prism.css"
